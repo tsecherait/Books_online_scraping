f960089abb73714cf6e42034991a14f5594c043a
Thomas SECHERAIT <tsecherait@hotmail.com>
2026-09-07 16:18:52 +0200

    loading all the elements of the dictionnary extracted

--- scraping_p1.py
+++ scraping_p1.py
@@ -1,5 +1,7 @@
 import requests
 import csv
+from bs4 import BeautifulSoup
+import re
 
 
 
@@ -7,17 +9,58 @@ def extract(url):
     response = requests.get(url)
     page = response.content
 
+    soup = BeautifulSoup(page, 'html.parser')
     product_page_url_to_transform = str(url)
-
+    tds=soup.findAll('td')
+        
+    universal_product_code_to_transform = tds[0].string
+    number_available_to_transform = tds[5].string
+    title_to_transform =soup.h1.text
+    price_including_tax_to_transform = tds[3].string
+    price_excluding_tax_to_transform = tds[2].string
+    product_description_to_transform = soup.find("meta", {"name": "description"}).get("content")
+    category_to_transform = soup.find("ul", class_="breadcrumb").find_all("li")[-2].find("a").text
+    review_rating_to_transform = soup.find('p', class_='star-rating')['class'][1]
+    image_url_to_transform = soup.find('article', class_='product_page').find("div").find("img").get("src")
     data_to_transform = {
-                'product_page_url_to_transform': product_page_url_to_transform,
+
+        'product_page_url_to_transform': product_page_url_to_transform,
+        'universal_product_code_to_transform': universal_product_code_to_transform,
+        'number_available_to_transform': number_available_to_transform,
+        'title_to_transform': title_to_transform,
+        'price_including_tax_to_transform': price_including_tax_to_transform,
+        'price_excluding_tax_to_transform': price_excluding_tax_to_transform,
+        'product_description_to_transform': product_description_to_transform,
+        'category_to_transform': category_to_transform,
+        'review_rating_to_transform': review_rating_to_transform,
+        'image_url_to_transform': image_url_to_transform,
+
     }
     return data_to_transform
 
 
 def transform(data_to_transform):
+    rating_mapping = {
+
+        'One': 1,
+        'Two': 2,
+        'Three': 3,
+        'Four': 4,
+        'Five': 5
+    }
     data_to_load = {
-            'product_page_url': data_to_transform['product_page_url_to_transform'],
+
+
+        'product_page_url': data_to_transform['product_page_url_to_transform'],
+        'universal_product_code': data_to_transform['universal_product_code_to_transform'],
+        'number_available': int(re.search(r'\d+', data_to_transform['number_available_to_transform']).group()),
+        'title': data_to_transform['title_to_transform'],
+        'price_including_tax': data_to_transform['price_including_tax_to_transform'][1:] + 'euros',
+        'price_excluding_tax': data_to_transform['price_excluding_tax_to_transform'][1:] + 'euros',
+        'product_description': data_to_transform['product_description_to_transform'].strip(),
+        'category': data_to_transform['category_to_transform'],
+        'review_rating': rating_mapping.get(data_to_transform['review_rating_to_transform'],None),
+        'image_url': 'product_page_url_to_transform'.rsplit('/', 2)[0] + '/' + data_to_transform['image_url_to_transform'],
     }
     return data_to_load
     
