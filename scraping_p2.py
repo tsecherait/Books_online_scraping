daa8ab229ffdc58e63194be217c6bbf9913e3a4b
Thomas SECHERAIT <tsecherait@hotmail.com>
2026-09-10 15:42:02 +0200

    add extract and transform category function

--- scraping_p2.py
+++ scraping_p2.py
@@ -1,9 +1,37 @@
-
-
+import requests
+from bs4 import BeautifulSoup
+from scraping_p1 import extract
+from scraping_p1 import transform
 
 
 def extract_and_transform_category(url):
-    pass
+    list_data_category = []
+    condition = True
+        #loop to create a list of dictionnaries whose values are books properties
+    while condition:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+    
+        books = soup.find_all('h3')
+        for book in books:
+            book_link = book.find('a')['href']
+                
+            data_book_category = transform(extract(url.rsplit('/', 4)[0] +'/'+ book_link.split('/', 3)[3]))
+            list_data_category.append(data_book_category)
+    
+        next_page = soup.find('li', class_='next')
+        if next_page:
+            next_page_link = next_page.find('a')['href']
+    
+                
+            next_page_url = url.rsplit('/',2)[0]+'/'+next_page_link
+            url = next_page_url
+            condition = url
+        else:
+            condition = next_page
+            
+    
+    return list_data_category
 
 
 def load_category(list_data_category):
