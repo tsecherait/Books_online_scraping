60cd88e819b387cb4792e99a34620bd5a5084d19
Thomas SECHERAIT <tsecherait@hotmail.com>
2026-09-15 14:07:57 +0200

    load images

--- scraping_p3.py
+++ scraping_p3.py
@@ -4,21 +4,30 @@ from bs4 import BeautifulSoup
 import os
 import csv
 
+    
 folder = "folder"
 
+
 def extract_and_load_images_category(all_category_books):
+    
     for category_name in all_category_books.keys():
         image_folder = os.path.join(folder, "images", category_name)
         if not os.path.exists(image_folder):
             os.makedirs(image_folder)
         for book_data in all_category_books[category_name]:
             image_url = book_data["image_url"]
-            image_name = book_data["universal_product_code"] + ".jpg"  # Nommez l'image en utilisant le code produit universel
+            image_name = book_data["universal_product_code"] + ".jpg"
             image_path = os.path.join(image_folder, image_name)
             response = requests.get(image_url)
             with open(image_path, 'wb') as image_file:
                 image_file.write(response.content)
-    
+                
+
+
+ 
+
+
+
 
 def load_all_category(all_category_books):
     file_folder = os.path.join(folder, "file")
@@ -32,15 +41,17 @@ def load_all_category(all_category_books):
             writer.writeheader()
             for book_data in all_category_books[category_name]:
                 writer.writerow(book_data)
-
+        
 
 def extract_all_category(url):
+    
+    
     response = requests.get(url)
     all_category_books = {}
-        
+    
     soup = BeautifulSoup(response.content, 'html.parser')   
     category_links = soup.find('ul', class_='nav').findAll('a')
-            
+        
     for category_link in category_links:
         category_url = url +'/'+ category_link['href']   
         category_name = category_url.split('/')[-2].split("_")[0]   
@@ -48,9 +59,6 @@ def extract_all_category(url):
             category_books_data = extract_and_transform_category(category_url)
             all_category_books.update({category_name: category_books_data})
     return all_category_books
-    
-    
-
 
 def main():
 
