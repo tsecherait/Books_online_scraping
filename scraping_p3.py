2be734506fb69c81207456dc9298839373202fcc
Thomas SECHERAIT <tsecherait@hotmail.com>
2026-09-15 13:39:03 +0200

    function for extraction

--- scraping_p3.py
+++ scraping_p3.py
@@ -1,3 +1,9 @@
+from scraping_p2 import extract_and_transform_category
+import requests
+from bs4 import BeautifulSoup
+
+
+
 def extract_and_load_images_category(all_category_books):
     pass
 
@@ -6,7 +12,21 @@ def load_all_category(all_category_books):
 
 
 def extract_all_category(url):
-    pass
+    response = requests.get(url)
+    all_category_books = {}
+        
+    soup = BeautifulSoup(response.content, 'html.parser')   
+    category_links = soup.find('ul', class_='nav').findAll('a')
+            
+    for category_link in category_links:
+        category_url = url +'/'+ category_link['href']   
+        category_name = category_url.split('/')[-2].split("_")[0]   
+        if category_name != "books":
+            category_books_data = extract_and_transform_category(category_url)
+            all_category_books.update({category_name: category_books_data})
+    return all_category_books
+    
+    
 
 
 def main():
